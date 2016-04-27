c508c5b24a752fa08e84c81de9346ce0140f729a
chenghsinhsu <chenghsin_hsu@trend.com.tw>
2016-04-27 14:14:33 +0800

    upload to s3 , create cloud formation stack

--- CloudFormation/deploy_cf.py
+++ CloudFormation/deploy_cf.py
@@ -1,14 +1,56 @@
 #!/usr/bin/env python 
 
+import time
+import sys
 import boto3
+import json
 
-cf = boto3.resource('cloudformation')
-for stack in cf.stacks.all():
-    print(stack)
+S3Bucket = "ch-cf-template"
 
-response = cf.meta.client.validate_template(
-    #TemplateBody='string',
-    TemplateURL='https://s3-us-west-2.amazonaws.com/ch-cf-template/lab1-vpc_ELB_combined.template'
-)
+def get_stack_info(cfClient, stackID):
+    response = cfClient.describe_stacks(StackName = stackID)
+    if len(response["Stacks"]) == 1:
+        return response["Stacks"][0]
+    else:
+        raise "STACK IS NOT FOUND"
 
-print response
+def wait_for_stack_created(cfClient,stackID):
+    stackInfo = get_stack_info(cfClient, stackID)
+    while stackInfo["StackStatus"] == "CREATE_IN_PROGRESS":
+        print("stack ID: " + stackID + " status: " + stackInfo["StackStatus"])
+        time.sleep(10)
+        stackInfo = get_stack_info(cfClient, stackID)
+        if(stackInfo["StackStatus"] == "CREATE_COMPLETE"):
+            print("stack " + stackInfo["StackName"] + " is created successfully")
+        else:
+            print("FAIL TO CREATE STACK " + stackID + " status: " + stackInfo["StackStatus"]);
+    sys.exit(1)
+
+def create_stack(cfClient,stackname, templateURL):
+    response = cfClient.create_stack(
+	StackName = stackname,
+	TemplateURL= templateURL,
+    )
+    print("StackID: " + response["StackId"])
+    return response["StackId"]
+
+def uploadcf(cftemplate):
+    s3client = boto3.client('s3')
+
+    #no status check?
+    s3client.upload_file(cftemplate, S3Bucket, cftemplate)
+    down_url = s3client.generate_presigned_url('get_object', \
+    	Params={'Bucket': S3Bucket, 'Key': cftemplate},ExpiresIn= 3600)
+    return down_url
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print 'Usage: python deploy_cf.py [cf template] [cf name]'
+        sys.exit(-1)
+
+    tmpurl = uploadcf(sys.argv[1])
+    
+    cfClient = boto3.client('cloudformation')
+    stackID = create_stack(cfClient,sys.argv[2], tmpurl)
+    wait_for_stack_created(cfClient, stackID)
+
